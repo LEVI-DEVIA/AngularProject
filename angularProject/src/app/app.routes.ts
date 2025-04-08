987a28c88b18870371973782ff349303c9e34125
Assale Levi <assalelevijonathanyao@gmail.com>
2025-04-08 23:59:51 +0000

    Update

--- angularProject/src/app/app.routes.ts
+++ angularProject/src/app/app.routes.ts
@@ -1,26 +1,29 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { LayoutComponent } from './pages/layout/layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { AddAssignmentComponent } from './assignments/add-assignment/add-assignment.component';
+import { NavigationErrorComponent } from './navigation-error-component/navigation-error-component.component';
+import { AssignmentDetailComponent } from './assignments/assignment-detail/assignment-detail.component';
+import { EditAssignmentComponent } from './assignments/edit-assignment/edit-assignment.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        redirectTo:'login',
-        pathMatch:'full'
-    },
-    {
-        path:'login',
-        component:LoginComponent
-    },
-    {
-        path:'',
-        component:LayoutComponent,
-        children:[
-            {
-                path:'dashboard',
-                component:DashboardComponent
-            }
-        ]
-    }
+    // Pour la page d'accueil
+    // On y accèdera avec l'URL : http://localhost:4200/home
+    // ou simplement http://localhost:4200
+    {path: '', component: AssignmentsComponent},
+    {path: 'home', component: AssignmentsComponent},
+    // Pour l'ajout d'assignments
+    // On y accèdera avec l'URL : http://localhost:4200/add
+    {path: 'add', component: AddAssignmentComponent},
+ // Pour le détail d'un assignment
+    // On y accèdera avec l'URL : http://localhost:4200/assignment/1 ou
+    // http://localhost:4200/assignment/2 ou ... avec 1 
+    // qui représente l'id de l'assignment
+    {path: 'assignments/:id', component: AssignmentDetailComponent},
+    // Pour la modification d'un assignment existant
+    {path: 'assignments/:id/edit', component: EditAssignmentComponent},
+   
+    // Pour l'erreur 404
+    // On y accèdera avec n'importe quelle URL qui ne correspond pas
+    // à une route définie
+    {path: '**', component:NavigationErrorComponent}
 ];
